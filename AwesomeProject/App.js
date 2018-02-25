2010d0e7b887aebff194d98da5e9bd4617ddd897
Agnaldo Marinho <agnaldoneto@ufpa.br>
2018-02-25 11:36:19 -0300

    Adcionando projetos inicias

---
+++ AwesomeProject/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+class App extends Component {
+  render() {
+      return (
+        <View>
+            <Header headerText= {'Albums!'} />
+            <AlbumList />
+        </View>
+      );
+  }
+};
+
+export default App;
+
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# reactJs-estudo
+# React Js
 Contém codigo e links para o estudo do reactjs
+
+### Instalando o cli do reactjs 
+
+
+# React Native
+### Instalando o cli do react native 
+
+### Configurando ambiente
+```
+sudo sysctl -w fs.inotify.max_user_instances=1024
+sudo sysctl -w fs.inotify.max_user_watches=12288
+```
+
